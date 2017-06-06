f0d81bc4313f6bcf26361ba8609f703c3a30aaa7
Justin Wendlandt <juswen@microsoft.com>
2017-06-06 12:13:46 -0700

    Adding server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+var restify = require('restify');
+var server = restify.createServer();
+
+server.get('/hello/:name', function (request, response, next) {
+    response.send('hello ' + request.params.name);
+    return next();
+});
+
+server.head('/hello/:name', function (request, response, next) {
+    response.send('hello ' + request.params.name);
+    return next();
+});
+
+server.post('/hello', function (request, response, next) {
+    response.send(201, Math.random().toString(36).substr(3, 8));
+    return next();
+});
+
+server.listen(8080, function() {
+    console.log('%s listening at %s', server.name, server.url);
+});
